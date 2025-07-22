b879e2763184cc2922d8cda48507ee7e3321b542
Antonis Mavritsakis <amavrits@gmail.com>
2025-07-22 16:55:52 +0200

    Finalized modularization of db building.

--- main/init_db.py
+++ main/init_db.py
@@ -59,81 +59,15 @@ if __name__ == "__main__":
 
                 add_sample_general_data(sample_name, borehole_name, project_name, general_data)
 
-                add_sample_test()
-
                 test_name = f"T_{sample_name}"
+                add_sample_test(test_name, sample_name, borehole_folder)
+
                 test_folder_list = [file for file in borehole_folder.iterdir() if file.name.split(".")[-1] != "json"]
-                test_name_list = [file.stem for file in test_folder_list]
-                has_str = not any((borehole_folder/"str").iterdir())
-                has_ftg = not any((borehole_folder/"ftg").iterdir())
-                has_stf = not any((borehole_folder/"stf").iterdir())
 
-                Test(
-                    sample=Sample(sample_name=sample_name),
-                    test_name=test_name,
-                    strength=has_str,
-                    fatigue=has_ftg,
-                    stiffness=has_stf,
-                )
+                for test_folder in test_folder_list:
 
-            for test_folder in test_folder_list:
-
-                df = pd.read_csv(test_folder/"raw_data.csv", index_col="sample_name")
-
-                with open(test_folder / "test_data.json", "r") as f:
-                    test_data = json.load(f)
-
-                for sample_name in df.index:
-
-                    data = df.loc[sample_name]
-
-                    if test_folder.stem == "str":
-
-                        StrSampleRaw(
-                            test=Test(test_name=test_name),
-                            notes=data["notes"],
-                            t=data["t"],
-                            F=data["F"],
-                            V_org=data["V_org"],
-                        )
-
-                    elif test_folder.stem == "ftg":
-
-                        FtgSampleRaw(
-                            test=Test(test_name=test_name),
-                            notes=data["notes"],
-                            N=data["N"],
-                            maximum_stroke=data["MaximumStroke"],
-                            minimum_stroke=data["MinimumStroke"],
-                            peak_to_peak_stroke=data["PeakToPeakStroke"],
-                            maximum_load=data["MaximumLoad"],
-                            peak_to_peak_load=data["PeakToPeakLoad"],
-                            in_phase_modulus=data["InPhaseModulus"],
-                            out_phase_modulus=data["OutPhaseModulus"],
-                        )
-
-                    elif test_folder.stem == "stf":
-
-                        EdynSampleRaw(
-                            test=Test(test_name=test_name),
-                            notes=data["notes"],
-                            T=data["T"],
-                            f=data["f"],
-                            eps=data["eps"],
-                            E_dyn=data["E_dyn"],
-                            pha=data["pha"]
-                        )
-
-                    else:
-                        raise ValueError(f"Unknown test type {test_folder.stem}")
-
-
-                    pass
-
-
-
-
-
+                    for data_type in ["raw", "processed"]:
+                        add_samples(test_name, test_folder, data_type)
 
     db.close()
 
--- src/db_builder/build.py
+++ src/db_builder/build.py
@@ -1,6 +1,8 @@
 import psycopg
 from peewee import PostgresqlDatabase, Model, CharField, ForeignKeyField
 from src.db_builder.models import *
+import pandas as pd
+import json
 
 
 def create_db(db_config):
@@ -123,6 +125,85 @@ def add_sample_general_data(sample_name, borehole_name, project_name, general_da
     )
 
 
+def add_sample_test(test_name, sample_name, borehole_folder):
+
+    has_str = not any((borehole_folder / "str").iterdir())
+    has_ftg = not any((borehole_folder / "ftg").iterdir())
+    has_stf = not any((borehole_folder / "stf").iterdir())
+
+    Test(
+        sample=Sample(sample_name=sample_name),
+        test_name=test_name,
+        strength=has_str,
+        fatigue=has_ftg,
+        stiffness=has_stf,
+    )
+
+
+def add_str_raw(test_name, test_data):
+    StrSampleRaw(
+        test=Test(test_name=test_name),
+        notes=test_data["notes"],
+        t=test_data["t"],
+        F=test_data["F"],
+        V_org=test_data["V_org"],
+    )
+
+
+def add_ftg_raw(test_name, test_data):
+    FtgSampleRaw(
+        test=Test(test_name=test_name),
+        notes=test_data["notes"],
+        N=test_data["N"],
+        maximum_stroke=test_data["MaximumStroke"],
+        minimum_stroke=test_data["MinimumStroke"],
+        peak_to_peak_stroke=test_data["PeakToPeakStroke"],
+        maximum_load=test_data["MaximumLoad"],
+        peak_to_peak_load=test_data["PeakToPeakLoad"],
+        in_phase_modulus=test_data["InPhaseModulus"],
+        out_phase_modulus=test_data["OutPhaseModulus"],
+    )
+
+
+def add_edyn_raw(test_name, test_data):
+    EdynSampleRaw(
+        test=Test(test_name=test_name),
+        notes=test_data["notes"],
+        T=test_data["T"],
+        f=test_data["f"],
+        eps=test_data["eps"],
+        E_dyn=test_data["E_dyn"],
+        pha=test_data["pha"]
+    )
+
+
+
+def add_str_processed(test_name, test_data):
+    StrSampleProcessed(
+        sample_raw=StrSampleRaw(test_name=test_name),
+        notes=test_data["notes"],
+        F=test_data["F"],
+        V_cor=test_data["V_cor"],
+        eps=test_data["eps"],
+        sig=test_data["sig"],
+        Sec=test_data["Sec"],
+    )
+
+
+def add_ftg_processed(test_name, test_data):
+    FtgSampleProcessed(
+        sample_raw=FtgSampleRaw(test_name=test_name),
+        notes=test_data["notes"],
+        N=test_data["N"],
+        eps_cycl=test_data["eps_cycl"],
+        eps_perm=test_data["eps_perm"],
+        sig_cyc=test_data["sig_cyc"],
+        sig_perm=test_data["sig_perm"],
+        E_dyn=test_data["E_dyn"],
+        pha=test_data["pha"],
+    )
+
+
 def iter_dikes(project_name, master_table, dike_table):
 
     project_master_table = master_table.loc[master_table["project"] == project_name]
@@ -136,6 +217,48 @@ def iter_dikes(project_name, master_table, dike_table):
         add_projectdike(dike_name, project_name)
 
 
+def add_samples(test_name, test_folder, data_type="raw"):
+
+    df = pd.read_csv(test_folder / f"{data_type}_data.csv", index_col="sample_name")
+
+    with open(test_folder / "test_data.json", "r") as f:
+        test_data = json.load(f)
+
+    for sample_name in df.index:
+
+        data = df.loc[sample_name]
+
+        if test_folder.stem == "str":
+
+            if data_type == "raw":
+                add_str_raw(test_name, data)
+            elif data_type == "processed":
+                add_str_processed(test_name, data)
+            else:
+                raise ValueError(f"Unknown data type: {data_type}")
+
+        elif test_folder.stem == "ftg":
+
+            if data_type == "raw":
+                add_ftg_raw(test_name, data)
+            elif data_type == "processed":
+                add_ftg_processed(test_name, data)
+            else:
+                raise ValueError(f"Unknown data type: {data_type}")
+
+        elif test_folder.stem == "stf":
+
+            if data_type == "processed":
+                continue
+
+            if data_type == "raw":
+                add_edyn_raw(test_name, data)
+            else:
+                raise ValueError(f"Unknown data type: {data_type}")
+
+        else:
+
+            raise ValueError(f"Unknown test type {test_folder.stem}")
 
 
 if __name__ == "__main__":
